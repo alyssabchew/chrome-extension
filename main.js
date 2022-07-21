e7f0d1745f8aa3de7bda985b7e0dbcda04006f6b
Alyssa Chew <alyssachew@MacBook-Pro-2.local>
2022-07-20 18:54:06 -0700

    images working sometimes

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // define images 
 // TODO: save all images from either slack or bio book
 // link them here in an array so we can use Math.random to access random array items and replace images
-const FTRIimages = ["./docs/assets/images/jared_bio_book.png"]; //["https://media-exp2.licdn.com/dms/image/C5603AQE9wR5a5MRIJw/profile-displayphoto-shrink_200_200/0/1641847254960?e=2147483647&v=beta&t=QbJoZJaVH0I53PJxq-sEb5tThIb-TkdX62lQTBfpdFU"];
-
+const FTRIimages = ["aiden", "annie", "cody", "corey", "demi", "jared", "kat", "kristin", "laura", "louie", "mia", "mike", "peaches", "phillip", "samantha", "shanda", "simon", "sophia", "xochilt", "zachary"]; 
 
 // use query selector to get all images off the DOM
 var browserImages = document.querySelectorAll("img");
@@ -11,12 +10,10 @@ var browserImages = document.querySelectorAll("img");
 // start with setting all images to jared
 
 // replace browserImages src's with FTRIimages
-console.log(FTRIimages[0]);
 for (let i = 0; i < browserImages.length; i++) {
+  const index = Math.floor(Math.random() * FTRIimages.length);
   browserImages[i].src = chrome.runtime.getURL(
-    FTRIimages[0]
+    "/docs/assets/images/" + FTRIimages[index] + ".png"
   );
 }
-console.log(chrome.runtime.getURL(
-  FTRIimages[0]
-))
+
